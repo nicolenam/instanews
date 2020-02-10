4770135a6bdfc7476e2d63e6268f0af99485f94b
nicole <eunjungnam@gmail.com>
2020-02-10 09:40:00 -0500

    feat: add text on top of image

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ $(function() {
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`
     }).done(function(data) {
-      console.log(data);
-      $.each(data, function(index, value) {
-        for (let i = 0; i < 11; i++) {
-          let multimedia = data.results[i].multimedia[2].url;
-          let abstract = data.results[i].abstract;
-          console.log(multimedia);
-          console.log(abstract);
-          $("#news").append(`<img src="${multimedia}"><br><span>${abstract}</span>`);
-        }
-      });
+      // console.log(data);
+      // $.each(data, function(index, value) {
+
+      // });
+      for (let i = 0; i <= 11; i++) {
+        let multimedia = data.results[i].multimedia[0].url;
+        let abstract = data.results[i].abstract;
+        let url = data.results[i].url;
+        // console.log(multimedia);
+        // console.log(abstract);
+        $(".news").append(
+          `<img class="thumbnail" src="${multimedia}"><br><p class="abstract"><a href="${url}">${abstract}</a></p>`
+        );
+      }
+      //find the position of the first <p>
+      //place the next <p> according to the first <p> position
+      let p = $(".link").first();
+      // console.log(p);
+      let position = p.position();
+      console.log(position)
+      // $(".link").append()
     });
   });
 });
