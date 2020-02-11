a828e4786149c3c9a8cbb933166665bd84ac395c
nicole <eunjungnam@gmail.com>
2020-02-11 10:00:31 -0500

    feat: add loader icon

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 $(function() {
-  $(".content").on("change", function(e) {
-    let section = $(".content option:selected").text();
+  $("#loader").hide();
 
+  $(".content").on("change", function(e) {
+    // let section = $(".content option:selected").text();
+  
+    let section = $(".content option:selected").val();
+    console.log(section);
+    $("#loader").show();
     $.ajax({
       dataType: "json",
       method: "GET",
       url: `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`
     }).done(function(data) {
+      $("#loader").hide();
       $(".news").empty();
+console.log(data)
       for (let i = 0; i <= 11; i++) {
         let multimedia = data.results[i].multimedia[0].url;
         let abstract = data.results[i].abstract;
